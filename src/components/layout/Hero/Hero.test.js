0548a185bb735dc168d158c21ade42c48eae9f54
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2020-03-02 13:35:47 +0100

    Add unit test Hero component with one unsolved bug

--- src/components/layout/Hero/Hero.test.js
+++ src/components/layout/Hero/Hero.test.js
@@ -2,11 +2,33 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Hero from  './Hero';
 
-// describe collect tests -- secound argument arrow function
+
 describe('Component Hero', () => {
-  //responsible for indyvidual test -- secound argument arrow function
   it('should render without crashing', () => {
-    const component = shallow(<Hero titleText='lorem'/>);
+    const component = shallow(<Hero titleText='Lorem ipsum' imageSrc='asasa' />);
     expect(component).toBeTruthy();
+    console.log(component.debug());
   });
+
+  it('should throw error withoud required props', () => {
+    expect(() => shallow(<Hero /> )).toThrow;
+  });
+
+  it('should render correct title & image', () => {
+    const expectedTitle = 'Lorem ipsum';
+    const expectedImage = 'image.jpg';
+    const component = shallow(<Hero titleText={expectedTitle} imageSrc={expectedImage} />);
+    const renderedTitle = component.find('.title').text();
+    expect(renderedTitle).toEqual(expectedTitle);
+    expect(component.find('.image').prop('src')).toEqual(expectedImage);
+  });
+
+  it('renders correct classNames', () => {
+    const mockVariants = 'small dummy';
+    const component = shallow(<Hero titleText='Lorem' imageSrc='image.jpg' variant={mockVariants} />);
+    expect(component.hasClass('component')).toBe(true);
+    expect(component.hasClass('small')).toBe(true);
+    expect(component.hasClass('dummy')).toBe(true);
+  });
+
 });
