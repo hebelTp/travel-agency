7e8785144fdbde5c0fcf32a2ba5da51187a19bd1
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2020-03-03 13:01:31 +0100

    Ad UT corect props

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -6,7 +6,23 @@ describe('Component TripSummary', () => {
   it('should render link to  correct adress', () =>{
     const correct = 'abc';
     const component = shallow(<TripSummary id={correct} tags={[]}/> ); //??
-    expect(component.find('.link').prop('to')).toEqual(`/trip/${correct}`);
+    const correctLink = component.find('.link').prop('to');
+    console.log(correctLink);
+    expect(correctLink).toEqual(`/trip/${correct}`);
     console.log(component.debug());
   });
+
+  it('render correct props', () => {
+    const expectedImage = 'image.jpg';
+    const expectedAlterText = 'name';
+
+    const component = shallow(<TripSummary tags={[]} image={expectedImage} name={expectedAlterText} />);
+
+    const renderedImage = component.find('img').prop('src') ;
+    const renderAlterText = component.find('img').prop('alt');
+
+    expect(renderedImage).toEqual(expectedImage);
+    expect(renderAlterText).toEqual(expectedAlterText);
+  });
+
 });
