ac0c438a9896eaa4711ebfbfef26e3026b7c2eae
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2020-03-09 13:02:13 +0100

    Add part of interaction test - simulate event change

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -18,6 +18,7 @@ describe ('OrderOption test ', () => {
     const component = shallow(<OrderOption name={expectedTitle} type='checkboxes' />);
     const renderedTitle = component.find('.title').text();
     expect(renderedTitle).toEqual(expectedTitle);
+    console.log(component.debug());
   });
 });
 
@@ -30,31 +31,97 @@ const optionTypes = {
   date: 'OrderOptionDate',
 };
 
-for(let type in optionTypes) {
-  describe(`Component OrderOption with type=${type}`, () => {
+/*  mixed options difrent properties from pircing.json */
+
+const mockProps = {
+  id: 'abc',
+  name: 'Lorem',
+  values: [
+    {id: 'aaa', icon: 'h-square', name: 'Lorem A', price: 0},
+    {id: 'xyz', icon: 'h-square', name: 'Lorem X', price: 100},
+  ],
+  required: false,
+  currentValue: 'aaa',
+  price: '50%',
+  limits: {
+    min: 0,
+    max: 6,
+  },
+};
+console.log('MOCK',[mockProps.id]);
+const mockPropsForType = {
+  dropdown: {},
+  icons: {},
+  checkboxes: {currentValue: [mockProps.currentValue]},
+  number: {currentValue: '1'},
+  text: {},
+  date: {},
+};
+
+const testValue = mockProps.values[1].id;
+const testValueNumber = 3;
+
+console.log(testValue);
+console.log(mockPropsForType);
+console.log(testValueNumber);
+
+for (let type in optionTypes) {
+  describe(`Component orderOption with type=${type}`, () => {
     /* test setup*/
     // each test will have actually rendered component
+
     let component;
+    let subcomponent;
+    let renderedSubcomponent;
+    let mockSetOrderOption; /* 1 */
+    //console.log(renderedSubcomponent);
 
     beforeEach(() => {
+      mockSetOrderOption = jest.fn(); /* 2 */
       component = shallow(
         <OrderOption
           type={type}
+          setOrderOption={mockSetOrderOption}
+          {...mockProps}
+          {...mockPropsForType[type]}
         />
       );
+      subcomponent = component.find(optionTypes[type]);
+      renderedSubcomponent = subcomponent.dive();
+      console.log(component.debug());
+      console.log(subcomponent.debug());
     });
 
 
     /* common tests */
-    it ('passes dummy test', () => {
-      expect(1).toBe(1);
-      console.log(component.debug());
+    it(`renders ${optionTypes[type]}`, () => {
+      expect(subcomponent).toBeTruthy();
+      expect(subcomponent.length).toBe(1);
+      console.log(subcomponent.length);
     });
 
     /* type-specific tests */
     switch (type) {
       case 'dropdown': {
         /* tests for dropdown */
+        it('contains select and options', () => {
+          const select = renderedSubcomponent.find('.select');
+          expect(select.length).toBe(1);
+
+          const emptyOption = select.find('option[value=""]').length;
+          expect(emptyOption).toBe(1);
+
+          const options=select.find('option').not('[value=""]');
+          expect(options.length).toBe(mockProps.values.length);
+          expect(options.at(0).prop('value')).toBe(mockProps.values[0].id);
+          expect(options.at(1).prop('value')).toBe(mockProps.values[1].id);
+        });
+
+        it('sholuld runsetOrderOption function on change', () => {
+          renderedSubcomponent.find('select').simulate('change', {currentTarget: {value: testValue}});
+          expect(mockSetOrderOption).toBeCalledTimes(1);
+          expect(mockSetOrderOption).toBeCalledWith({[mockProps.id]: testValue});
+        });
         break;
       }
     }
